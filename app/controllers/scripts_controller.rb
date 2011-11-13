edf2fa6b46ca46dd2967e94c39a328ab39e40dc0
Matt Van Veenendaal <m@mattvv.com>
2011-11-12 17:01:17 -0800

    adding scripts and lines

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -1,11 +1,22 @@
 class ScriptsController < ApplicationController
   before_filter :require_user
 
+  def create
+    if params[:id]
+      Script.add_line(params[:id], params[:character], params[:line])
+      redirect_to script_path(params[:id])
+    else
+      Script.add_script(params[:name], @current_user.username)
+      redirect_to scripts_path
+    end
+  end
+
   def index
     @scripts = Script.scripts(@current_user.username)
   end
 
   def show
-    @lines = Script.lines(params[:id])
+    @script = params[:id]
+    @lines = Script.lines(@script)
   end
 end
--- app/models/script.rb
+++ app/models/script.rb
@@ -2,7 +2,7 @@ class Script
   def self.scripts(username)
     site = RestClient::Resource.new SITE, APPLICATION_ID, MASTER_KEY
     begin
-      response = site["/classes/Script"].get({:params => {:where => {"username" => username }.to_json}})
+      response = site["/classes/Script"].get({:params => {:order => "-createdAt", :where => {"username" => username }.to_json}})
       JSON.parse(response)["results"]
     rescue Exception => e
       false
@@ -15,6 +15,26 @@ class Script
       response = site["/classes/Line"].get({:params => {:order => "createdAt", :where => {"scriptId" => script_id }.to_json}})
       JSON.parse(response)["results"]
     rescue Exception => e
+      false
+    end
+  end
+
+  def self.add_script(name, username)
+    site = RestClient::Resource.new SITE, APPLICATION_ID, MASTER_KEY
+    begin
+      response = site["/classes/Script"].post({"name"=>name, "username" => username}.to_json, :content_type => 'application/json', :accept => :json)
+      !JSON.parse(response)["objectId"].blank?
+    rescue Exception
+      false
+    end
+  end
+
+  def self.add_line(script_id, character, line)
+    site = RestClient::Resource.new SITE, APPLICATION_ID, MASTER_KEY
+    begin
+      response = site["/classes/Line"].post({"scriptId"=>script_id, "character" => character, "line" => line}.to_json, :content_type => 'application/json', :accept => :json)
+      !JSON.parse(response)["objectId"].blank?
+    rescue Exception
       false
     end
   end
