2c5750ff631f6fe48959bfd62199f11e7b251d63
Matt Van Veenendaal <m@mattvv.com>
2012-02-01 23:58:56 -0800

    Add script return

--- app/models/script.rb
+++ app/models/script.rb
@@ -29,6 +29,16 @@ class Script
     end
   end
 
+  def self.add_script_return(name, username)
+    site = RestClient::Resource.new SITE, APPLICATION_ID, MASTER_KEY
+    begin
+      response = site["/classes/Script"].post({"name"=>name, "username" => username}.to_json, :content_type => 'application/json', :accept => :json)
+      JSON.parse(response)["objectId"]
+    rescue Exception
+      false
+    end
+  end
+
   def self.add_line(script_id, character, line)
     site = RestClient::Resource.new SITE, APPLICATION_ID, MASTER_KEY
     begin
